5e688f5b89f7392930865c36f8211444c0aa2d5c
Jacek Kowalczyk <j.kowalczyk@clearcode.cc>
2018-03-07 10:09:30 +0100

    [PPTECHDOC-19 PPTECHDOC-7] CSS and JS cleanup

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,21 +1,5 @@
 (function () {
     window.onload = function () {
-        var tocList = document.querySelectorAll('.wy-side-scroll p.caption');
-        var tocUlList = document.querySelectorAll('.wy-side-scroll ul');
-        for (var i = 0; i < tocList.length; i++) {
-            tocList[i].addEventListener('click', function(event) {
-                for (var i = 0; i < tocList.length; i++) {
-                    tocList[i].classList.remove("active-toc");
-                }
-                event.target.parentElement.classList.add("active-toc");
-            });
-        }
-        for (i = 0; i < tocUlList.length; i++) {
-            var current = tocUlList[i];
-            if(current.classList.contains('current')) {
-                current.previousSibling.previousSibling.classList.add("active-toc");
-            }
-        }
         var isApiPresent = document.getElementsByTagName('redoc').length;
         if(isApiPresent) {
             window.addEventListener("hashchange", function(){
