c91de9dd13fa9ada8ef574bfccefd4d8debca1b1
Jacek Kowalczyk <j.kowalczyk@clearcode.cc>
2018-03-16 10:50:47 +0100

    [PPTECHDOC-19] Fixes

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -3,14 +3,8 @@
         var isApiPresent = document.getElementsByTagName('redoc').length;
         if(isApiPresent) {
             window.addEventListener("hashchange", function(){
-                var locationHash = location.hash.split('#')[1],
-                    elementToScroll = null;
-                if(locationHash.split('/')[0] !== 'section'&& locationHash.split('/')[0] !== 'tag' ) {
-                    elementToScroll = document.querySelector('[href="'+ location.hash +'"]');
-                }
-                if(elementToScroll){
+                    var elementToScroll = document.querySelector('[href="'+ location.hash +'"]');
                     elementToScroll.scrollIntoView();
-                }
             }, false);
         }
     }
--- audience_manager/public_api/audience_manager_api.rst
+++ audience_manager/public_api/audience_manager_api.rst
@@ -7,5 +7,5 @@ Audience Manager Public API
 
 .. raw:: html
 
-    <redoc spec-url="../../_static/api/audience_manager_public_api.json" expand-responses="">
+    <redoc spec-url="../../_static/api/audience_manager_public_api.json" expand-responses="" sticky-sidebar="">
     </redoc>
