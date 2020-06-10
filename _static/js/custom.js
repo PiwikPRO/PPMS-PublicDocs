dbcb036ced8e881d9c8df3568c9a1439af008208
Jacek Kurek <j.kurek@piwik.pro>
2020-06-10 07:44:05 +0200

    PPTECHDOC-71 detection, fixed image

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -14,5 +14,17 @@
     //Without it custom.css is introduced before theme.css and is overwritten
     var style = document.querySelector('link[href$="/custom.css"]');
     var head = document.querySelector('head');
+    var detectedUnicorn = function () {
+        let element = document.createElement('div');
+        element.className = 'unicorn-found';
+        element.textContent = 'Your browser is using an ad blocker that affects the UI of this website. Turn it off or add this website to the whitelist';
+        let injectTo = document.querySelectorAll('.wy-nav-content');
+        if(injectTo.length === 1) {
+            injectTo[0].parentElement.insertBefore(element, injectTo[0]);
+        }
+    };
     head.appendChild(style.cloneNode());
+    if(typeof window.detectionOfStuff !== 'undefined' && typeof unicornDetector !== 'undefined') {
+        window.unicornDetector.onDetected(detectedUnicorn);
+    }
 })();
