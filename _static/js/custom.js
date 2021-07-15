356dcca37b17b748108baedbee03eacad2c37751
Jacek Kurek <j.kurek@piwik.pro>
2021-07-15 10:11:29 +0200

    PPNA-3924 gap fix 4

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -2,7 +2,7 @@
     //Hack to fix issue PPTECHDOC-54
     //RTD is building docs differently since ~ 22-03-2019
     //Without it custom.css is introduced before theme.css and is overwritten
-    var style = document.querySelector('link[href$="/custom_1626335939764.css"]');
+    var style = document.querySelector('link[href$="/custom_1626336655663.css"]');
     var head = document.querySelector('head');
     var detectedUnicorn = function () {
         let element = document.createElement('div');
