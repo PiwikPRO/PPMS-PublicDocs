6e9e85ab5b90cbf17d354674ff0382256d8a5b95
Jacek Kurek <j.kurek@piwik.pro>
2022-02-14 08:42:32 +0100

    PPTECHDOC-87 add version js

---
+++ _static/js/versionlinks.js
@@ -0,0 +1,16 @@
+//CHANGE VERSION BEFORE NEW RELEASE
+if(document.location.pathname.indexOf('/en/latest') === 0) {
+    document.querySelector('.rst-current-version').innerHTML = document.querySelector('.rst-current-version').innerHTML.replace('v: latest', 'v: latest (16.11)')
+}
+
+if(document.querySelector('.injected')){
+    document.querySelector('.injected > dl:nth-child(1) > dd:nth-child(2) > a').innerText = 'latest (16.11)';
+} else {
+    let observer = new MutationObserver(() => {
+        if (document.querySelector('.injected')) {
+            document.querySelector('.injected > dl:nth-child(1) > dd:nth-child(2) > a').innerText = 'latest (16.11)';
+            observer.disconnect();
+        }
+    });
+    observer.observe(document.querySelector('.rst-versions.lower-menu'), {childList : true, subtree: true});
+}
