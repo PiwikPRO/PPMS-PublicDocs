54138dc2844a36ccf8b73e8dfb3af4fa862a31b2
Jacek Kurek <j.kurek@piwik.pro>
2022-03-08 11:20:51 +0100

    PPTECHDOC-87 bump version

--- conf.py
+++ conf.py
@@ -89,7 +89,7 @@ author = u'Piwik PRO'
 # built documents.
 #
 # The short X.Y version.
-version = u'16.13'
+version = u'16.14'
 # The full version, including alpha/beta/rc tags.
 release = version
 
