141583c6a9e6b203c16e0e21e29203b375034514
Jacek Kurek <j.kurek@piwik.pro>
2023-01-31 10:46:41 +0100

    PPTECHDOC-87 versions

--- conf.py
+++ conf.py
@@ -90,7 +90,7 @@ author = u'Piwik PRO'
 # built documents.
 #
 # The short X.Y version.
-version = u'16.34'
+version = u'16.35'
 # The full version, including alpha/beta/rc tags.
 release = version
 
